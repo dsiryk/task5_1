3b86f2d1fa80aae846854f559bbe622e425875cf
dezag <98271110+dsiryk@users.noreply.github.com>
2025-02-27 17:53:13 +0200

    Update Program.cs

--- task5_2/Program.cs
+++ task5_2/Program.cs
@@ -1,10 +1,8 @@
-﻿
-
+using System;
 
 class Sad
 {
     static Random random = new Random();
-
 
     static int[] generateArray(int arraySize, int minValue, int maxValue, Random random)
     {
@@ -24,45 +22,53 @@ class Sad
         {
             Console.Write(array[i] + " ");
         }
+        Console.WriteLine();
     }
+
     static (int min, int max) find(int[] array)
     {
-        int min = array[0]; 
-        int max = array[0]; 
+        int min = array[0];
+        int max = array[0];
 
-        for (int i = 1; i < array.Length; i++) 
+        for (int i = 1; i < array.Length; i++)
         {
             if (array[i] < min)
             {
-                min = array[i]; 
+                min = array[i];
             }
             if (array[i] > max)
             {
-                max = array[i]; 
+                max = array[i];
             }
-
         }
 
         return (min, max);
     }
 
-
     static void Main(string[] args)
     {
-      
         int[] list = generateArray(100, 100, 901, random);
-        var (min, max) = find(list);
-        Console.WriteLine($"Найменше число: {min}");
-        Console.WriteLine($"Найбільше число: {max}");
 
-        
+      
+        Console.Write("Числа більше 500: ");
+        for (int i = 0; i < list.Length; i++)
+        {
+            if (list[i] > 500)
+            {
+                Console.Write(list[i] + " ");
+            }
+        }
+
+       
         int sum = 0;
         for (int i = 0; i < list.Length; i++)
         {
             sum += list[i];
         }
+        Console.WriteLine("\nСума всіх чисел масиву: " + sum);
 
-        Console.Write("Числа кратні п'яти: ");
+        
+        Console.Write("Числа кратні 5: ");
         for (int i = 0; i < list.Length; i++)
         {
             if (list[i] % 5 == 0)
@@ -71,35 +77,22 @@ class Sad
             }
         }
 
-        Console.Write("\n Числа більше 500: ");
-        for (int i = 0; i < list.Length; i++)
-        {
-            if (list[i] > 500)
-            {
-                Console.Write(list[i] + " ");
-             
-            }
-        }
-        
-        int count = 0;
+        var (min, max) = find(list);
+        Console.WriteLine("");
+        Console.WriteLine("Найменше число:" + min);
+        Console.WriteLine("Найбільше число:" + max);
 
+
+        int count = 0;
         for (int i = 0; i < list.Length; i++)
         {
             if (list[i] >= 500 && list[i] <= 600)
             {
                 count++;
-              
             }
         }
-        Console.WriteLine(" ");
+       
         Console.WriteLine("Числа в межах вiд 500 до 600: " + count);
 
-
-
-
-
-
-        Console.WriteLine("Сумма всіх чисел масива: " + sum);
     }
-
 }
